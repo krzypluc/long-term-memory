97d6e75472c5da5d8e36820f3d98247e7d775d54
Krzysztof P <krzypluc@gmail.com>
2023-02-27 15:33:08 +0100

    update

--- memory-frontend/src/routes/Tile.svelte
+++ memory-frontend/src/routes/Tile.svelte
@@ -15,7 +15,7 @@
             <p>
               <strong>{row.firstname} {row.surname}</strong> <small>@johnsmith</small> <small>31m</small>
               <br>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+              {row.description}
             </p>
           </div>
         </div>
